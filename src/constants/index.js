97986491553015ad995fc159eba9e9097d30f92d
Mohammad Abbasi <mohammad@Mohammads-MacBook-Pro.local>
2025-03-15 09:21:59 -0700

    Upload resume

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,7 +21,7 @@ export const HERO_CONTENT = {
   description:
     "I’m currently helping businesses bring their visions to life through interactive digital solutions.",
   resumeLinkText: "Download Resume",
-  resumeLink: "/MohammadAbbasiResume 2.pdf",
+  resumeLink: "/resume.pdf",
 };
 
 export const PROJECTS = [
