d0f1dfc18991a9acb3ab6aededb21fefef8325be
Moe <moe.abbasi1983@gmail.com>
2026-04-27 19:37:51 -0700

    Update my resume

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,7 +21,7 @@ export const HERO_CONTENT = {
   description:
     "I’m currently helping businesses bring their visions to life through interactive digital solutions.",
   resumeLinkText: "Download Resume",
-  resumeLink: "/MohammadAbbasiResumeUp.pdf",
+  resumeLink: "/MohammadCV.pdf",
 };
 
 export const PROJECTS = [
